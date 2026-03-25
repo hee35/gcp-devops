f0085f5925043dfdbe7a16959bf76fe83b02a579
kim <example@gmail.com>
2026-03-25 04:38:16 +0000

    unzip

---
+++ repo-3/main.py
@@ -0,0 +1,8 @@
+def funcion_cicd(request):
+	request_json = request.get_json()
+	if request.args and 'message' in request.args:
+		return request.args.get('message')
+	elif request_json and 'message' in request_json:
+		return request_json['message']
+	else:
+		return f'Function - 1 with V1.0 with CI/CD Pipeline'
