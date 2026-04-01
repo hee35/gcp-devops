cfdba6b80adbc682ddc99e72c699fbd21d1a3cf9
kim <example@gmail.com>
2026-04-01 08:25:28 +0000

    v2.0

--- repo-5/main.py
+++ repo-5/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Welcome to Python Flask World v1.0 && from CICD GKE'
+	return 'Welcome to Python Flask World v2.0 && from CICD GKE'
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080)
